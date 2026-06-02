6c56b15fd341601005e8e1ce30541a5c3b7a8833
Brandon Awan <abibangbrandon855@gmail.com>
2026-06-02 17:36:37 +0100

    updated file of deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -10,7 +10,7 @@ git pull origin "$BRANCH"
 
 # Build the Docker images
 echo "Building Docker images..."
-if ! docker compose build --no-cache; then
+if ! docker-compose build; then
     echo "Failed to build Docker images."
     exit 1
 fi
