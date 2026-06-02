6e78dbb9ac6067c547b7e099142c78a8bf497c58
Brandon Awan <abibangbrandon855@gmail.com>
2026-06-02 18:26:25 +0100

    updates the entryscript

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,14 +7,14 @@ POSTGRES_HOST=${POSTGRES_HOST:-db}
 POSTGRES_PORT=${POSTGRES_PORT:-5432}
 POSTGRES_USER=${POSTGRES_USER:-postgres}
 
-# Wait for PostgreSQL to be ready
+# Wait for PostgreSQL to be ready (Explicitly using -d postgres)
 echo "Waiting for PostgreSQL to be ready..."
-until pg_isready -h "$POSTGRES_HOST" -p "$POSTGRES_PORT" -U "$POSTGRES_USER"; do
+until pg_isready -h "$POSTGRES_HOST" -p "$POSTGRES_PORT" -U "$POSTGRES_USER" -d postgres; do
   echo "Waiting for database connection..."
   sleep 2
 done
 
-# Check if the database exists
+# Check if the database exists (Explicitly using -d postgres)
 echo "Checking if the database exists..."
 psql -h "$POSTGRES_HOST" -U "$POSTGRES_USER" -d postgres -c "SELECT 1 FROM pg_database WHERE datname='$POSTGRES_DB'" | grep -q 1 || {
   echo "Database does not exist. Creating database..."
@@ -28,7 +28,6 @@ python manage.py migrate --noinput
 # Collect static files
 echo "Collecting static files..."
 python manage.py collectstatic --noinput
-
 
 # Start the server
 echo "Starting server..."
