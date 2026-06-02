b6b89a497167b4524d929f8005676fdf568700a8
Brandon Awan <abibangbrandon855@gmail.com>
2026-06-02 19:11:05 +0100

    changes

--- core/asgi.py
+++ core/asgi.py
@@ -4,6 +4,12 @@ ASGI config for the core project.
 Routes HTTP requests to Django and WebSocket connections to Channels consumers.
 """
 import os
+
+# Workaround for corrupted channels package in some environments.
+import channels as _channels
+if not hasattr(_channels, "DEFAULT_CHANNEL_LAYER"):
+    _channels.DEFAULT_CHANNEL_LAYER = "default"
+
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
@@ -12,8 +18,10 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
 
 django_asgi = get_asgi_application()
 
-# Import routing after django initialization
 import chats.routing  # noqa: E402
+# import archive.notifications.routing  # noqa: E402
+
+combined_routes = chats.routing.websocket_urlpatterns  # + archive.notifications.routing.websocket_urlpatterns
 
 application = ProtocolTypeRouter({
     "http": django_asgi,
@@ -21,36 +29,4 @@ application = ProtocolTypeRouter({
         URLRouter(chats.routing.websocket_urlpatterns)
     ),
 })
-
-# """
-# ASGI config for the core project.
-
-# Routes HTTP requests to Django and WebSocket connections to Channels consumers.
-# """
-# import os
-
-# # Workaround for corrupted channels package in some environments.
-# import channels as _channels
-# if not hasattr(_channels, "DEFAULT_CHANNEL_LAYER"):
-#     _channels.DEFAULT_CHANNEL_LAYER = "default"
-
-# from channels.auth import AuthMiddlewareStack
-# from channels.routing import ProtocolTypeRouter, URLRouter
-# from django.core.asgi import get_asgi_application
-
-# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
-
-# django_asgi = get_asgi_application()
-
-# import chats.routing  # noqa: E402
-# # import archive.notifications.routing  # noqa: E402
-
-# combined_routes = chats.routing.websocket_urlpatterns  # + archive.notifications.routing.websocket_urlpatterns
-
-# application = ProtocolTypeRouter({
-#     "http": django_asgi,
-#     "websocket": AuthMiddlewareStack(
-#         URLRouter(chats.routing.websocket_urlpatterns)
-#     ),
-# })
 
