1d2b46c9750fa28c1ffac7cdb45db47e130446ea
Brandon Awan <abibangbrandon855@gmail.com>
2026-06-02 23:06:28 +0100

    updated the deploy.sh file

--- deploy.sh
+++ deploy.sh
@@ -29,36 +29,36 @@ if ! docker-compose up -d; then
     exit 1
 fi
 
-# echo "Waiting for Django staticfiles to be collected by container..."
-# for i in {1..30}; do
-#     if [ -d "./staticfiles/admin" ]; then
-#         echo "Static files found!"
-#         break
-#     fi
-#     echo "⏳ Waiting for staticfiles... ($i/30)"
-#     sleep 3
-# done
+echo "Waiting for Django staticfiles to be collected by container..."
+for i in {1..30}; do
+    if [ -d "./staticfiles/admin" ]; then
+        echo "Static files found!"
+        break
+    fi
+    echo "⏳ Waiting for staticfiles... ($i/30)"
+    sleep 3
+done
 
-# # If staticfiles still not found, exit with error
-# if [ ! -d "./staticfiles/admin" ]; then
-#     echo "Static files were not collected in time. Aborting sync to Nginx."
-#     exit 1
-# fi
+# If staticfiles still not found, exit with error
+if [ ! -d "./staticfiles/admin" ]; then
+    echo "Static files were not collected in time. Aborting sync to Nginx."
+    exit 1
+fi
 
-# # Sync static files to Nginx path
-# echo "Syncing static files to Nginx..."
-# rm -rf /var/www/2geda/staticfiles
-# cp -r ./staticfiles /var/www/2geda/staticfiles
-# chmod -R o+r /var/www/2geda/staticfiles
-# find /var/www/2geda/staticfiles -type d -exec chmod o+x {} \;
+# Sync static files to Nginx path
+echo "Syncing static files to Nginx..."
+rm -rf /var/www/2geda/staticfiles
+cp -r ./staticfiles /var/www/2geda/staticfiles
+chmod -R o+r /var/www/2geda/staticfiles
+find /var/www/2geda/staticfiles -type d -exec chmod o+x {} \;
 
-# # Reload Nginx
-# echo "Reloading Nginx..."
-# if nginx -t && systemctl reload nginx; then
-#     echo "Nginx reloaded successfully."
-# else
-#     echo "Nginx reload failed."
-#     exit 1
-# fi
+# Reload Nginx
+echo "Reloading Nginx..."
+if nginx -t && systemctl reload nginx; then
+    echo "Nginx reloaded successfully."
+else
+    echo "Nginx reload failed."
+    exit 1
+fi
 
 echo "🚀 Services started successfully."
